2632d135bb5e03db87f708996dc33c1b586f177e
Julian Nicholls <juliannicholls29@gmail.com>
2018-05-04 15:44:30 +0100

    Add and update local interface modules

--- campaign/ethereum/factory.js
+++ campaign/ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import campaignFactory from './build/CampaignFactory.json';
 
-const address = '0x045FEBAA2e29cFE938701075e9b2B64a745FeCdf';
+const address = '0x6a79b947702e43A465b2fc49AC867Df8c7A95Baa';
 const factory = new web3.eth.Contract(
   JSON.parse(campaignFactory.interface),
   address
